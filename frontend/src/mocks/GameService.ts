f94ce5f6790e134a606990f3b7f7f99ff227b024
O3H <owen77stubbs@gmail.com>
2024-09-21 18:44:40 +0100

    unignore GameService mock

---
+++ frontend/src/mocks/GameService.ts
@@ -0,0 +1,68 @@
+import { ThunderstoreGame } from "@types"
+
+// TODO: Replace with a call to a real service.
+export const getGameList = (): ThunderstoreGame[] => [{
+  title: "Lethal Company",
+  identifier: 'lethal-company',
+  image: "LethalCompany.png",
+  path: "E:\\SteamLibrary\\steamapps\\common\\Lethal Company",
+  aliases: ["LC", "LethalCompany"],
+  steamID: 1966720
+}, {
+  title: "Risk of Rain 2",
+  identifier: 'riskofrain2',
+  image: "RiskOfRain2.jpg",
+  path: "H:\\Program Files (x86)\\Steam\\steamapps\\common\\Risk of Rain 2",
+  aliases: ["ror2"],
+  steamID: 248820
+}, {
+  title: "Content Warning",
+  identifier: 'content-warning',
+  image: "ContentWarning.png",
+  path: "H:\\Program Files (x86)\\Steam\\steamapps\\common\\Content Warning",
+  aliases: ["CW", "ContentWarning"],
+  steamID: 0
+}, {
+  title: "Palworld",
+  identifier: 'palworld',
+  image: "Palworld.png",
+  path: "H:\\Program Files (x86)\\Steam\\steamapps\\common\\Palworld",
+  aliases: ["Palworld", "Pal"],
+  steamID: 0
+}, {
+  title: "Valheim",
+  identifier: 'valheim',
+  image: "Valheim.jpg",
+  steamID: 0
+}, {
+  title: "Subnautica",
+  identifier: 'subnautica',
+  image: 'Subnautica.png',
+  steamID: 0
+}, {
+  title: "Dredge",
+  identifier: 'dredge',
+  image: 'Dredge.png',
+  steamID: 0
+}, {
+  title: 'Techtonica',
+  identifier: 'techtonica',
+  image: 'techtonica.png',
+  steamID: 0
+}, {
+  title: 'Placeholder',
+  identifier: 'tf2',
+  steamID: 0
+}, {
+  title: 'Placeholder',
+  identifier: 'tf3',
+  steamID: 0
+}, {
+  title: 'Placeholder',
+  identifier: 'tf4',
+  steamID: 0
+}, {
+  title: 'Placeholder',
+  identifier: 'tf5',
+  steamID: 0
+}]

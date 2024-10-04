1a64d9e0fa10a4613e76eaec3006d42ca28daeb2
O3H <owen77stubbs@gmail.com>
2024-10-04 03:02:03 +0100

    generate project.nsi

--- frontend/src/components/general/ControlButtons.vue
+++ frontend/src/components/general/ControlButtons.vue
@@ -7,23 +7,33 @@ import {
     WindowIsMaximised,
 } from '@runtime'
 
-const CustomMinimise = async () => await WindowIsMaximised() 
-    ? WindowUnmaximise()
-    : WindowMaximise()
+import ButtonGroup from 'primevue/buttongroup'
+
+let windowMaximised: boolean
+
+const CustomMinimise = async () => {
+    const maximised = await WindowIsMaximised() 
+    if (maximised) WindowUnmaximise()
+    else WindowMaximise()
+
+    windowMaximised = maximised
+}
 </script>
 
 <template>
 <div class="control-buttons no-drag">
     <ButtonGroup>
-        <Button plain text icon="pi pi-minus" @click="WindowMinimise"/>
-        <Button plain text icon="pi pi-expand" @click="CustomMinimise"/>
-        <Button plain text icon="pi pi-times" @click="Quit"/>
+        <Button text icon="pi pi-minus" @click="WindowMinimise"/>
+        <Button text :icon="`pi ${windowMaximised ? 'pi-minimise' : 'pi-expand'}`" @click="CustomMinimise"/>
+        <Button text icon="pi pi-times" @click="Quit"/>
     </ButtonGroup>
 </div>
 </template>
 
 <style scoped>
 .control-buttons {
+    margin-top: 4px;
+    margin-right: 4px;
     z-index: 9999;
     position: fixed;
     top: 0;
@@ -36,14 +46,18 @@ const CustomMinimise = async () => await WindowIsMaximised()
     /* background-color: rgb(23, 23, 26); */
 }
 
+.control-buttons * {
+    background-color: transparent;
+}
+
 .control-buttons .p-button {
     cursor: pointer;
     color: rgba(201, 201, 201, 0.905);
-    width: 42px;
-    height: 36px;
+    width: 40px;
 }
 
 .control-buttons :hover {
     color: rgba(166, 105, 246, 0.863);
+    background-color: transparent;
 }
 </style>

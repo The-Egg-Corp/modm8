6141168865a784d130e68b311210f30c52b4208b
O3H <owen77stubbs@gmail.com>
2024-10-23 01:26:21 +0100

    move old Thunderstore stuff into its new place.

--- frontend/src/components/reusable/PlatformSelectPanels.vue
+++ frontend/src/components/reusable/PlatformSelectPanels.vue
@@ -1,40 +1,7 @@
 <script lang="ts" setup>
-import { ref } from "vue"
-
-// import FileUpload from 'primevue/fileupload'
-// import DataView from 'primevue/dataview'
-
 import Splitter from 'primevue/splitter'
 import SplitterPanel from 'primevue/splitterpanel'
 //import OutlineHoverButton from "./OutlineHoverButton.vue"
-
-import { TSPackageManifest, TSPackageFile } from '@types'
-import { experimental } from '@frontend/wailsjs/go/models'
-
-// TODO: Finish implementing pack/unpack functionality
-import { 
-    //UnpackZip, 
-    ValidateIcon, 
-    ValidateManifest, 
-    ValidateReadme
-} from '@backend/thunderstore/Tools'
-
-// Validates each required file for uploading a Thunderstore mod.
-function ValidateFiles(icon: experimental.IconValidatorParams, readme: TSPackageFile, manifest: TSPackageManifest) {
-    ValidateIcon(icon)
-    ValidateReadme(readme.data)
-
-    if (!manifest.author) return
-    ValidateManifest(manifest.author, manifest.data)
-}
-
-const items = ref([{ 
-    label: 'Your Packages', 
-    icon: 'pi pi-box' 
-}, {
-    label: 'Package Validater',
-    icon: 'pi pi-check-square' 
-}])
 
 const props = defineEmits(['selectThunderstore', 'selectNexus'])
 </script>
@@ -64,33 +31,6 @@ const props = defineEmits(['selectThunderstore', 'selectNexus'])
         </div>
     </SplitterPanel>
 </Splitter>
-
-<!-- <div class="mod-dev-tools column">
-    <DataView dataKey="dev-package-list">
-
-    </DataView>
-</div>
-
-<div class="no-drag">
-    <FileUpload
-        name="demo[]"
-        accept=".zip"
-        :multiple="false"
-        :maxFileSize="1000000"
-        :showUploadButton="false"
-        @select=""
-    >
-        <template #content>
-
-        </template>
-
-        <template #empty>
-            <div class="flex row pi pi-upload gap-2">
-                <div>Drag and drop your mod zip file here.</div>
-            </div>
-        </template>
-    </FileUpload>
-</div> -->
 </template>
 
 <style scoped>
@@ -146,10 +86,5 @@ const props = defineEmits(['selectThunderstore', 'selectNexus'])
 
 :deep(.p-splitter-gutter-handle) { 
     background: rgba(255, 255, 255, 0.617) !important;
-}
-
-.mod-dev-tools {
-    margin-left: 75px; /* Account for sidebar */
-    top: 350px;
 }
 </style>

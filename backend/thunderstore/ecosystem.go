da84149ae2cd0720ae2a722051ca83fcfe0d97a8
O3H <owen77stubbs@gmail.com>
2025-06-25 21:41:01 +0100

    improvements to `ecosystem.go`
    - Fixed issue where Decode was used only once, just replaced with unmarshal.
    - Removed redundant assignment to ecosystem in NewThunderstoreSchema().
    - GetEcosystem() now able to fall back to ecosystem.json file.

--- backend/thunderstore/ecosystem.go
+++ backend/thunderstore/ecosystem.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
+	"modm8/backend/common/fileutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 const (
@@ -31,57 +33,113 @@ type ThunderstoreSchema struct {
 func NewThunderstoreSchema() *ThunderstoreSchema {
 	schema := &ThunderstoreSchema{}
 
-	ecosys, err := schema.GetEcosystem()
+	_, err := schema.GetEcosystem()
 	if err != nil {
 		return nil
 	}
 
-	schema.ecosystem = ecosys
 	return schema
 }
 
-func (schema *ThunderstoreSchema) Validated() bool {
-	return schema.validated
-}
+// func (schema *ThunderstoreSchema) Validated() bool {
+// 	return schema.validated
+// }
 
 func (schema *ThunderstoreSchema) GetEcosystem() (*ThunderstoreEcosystem, error) {
 	if schema.validated {
 		return schema.ecosystem, nil
 	}
 
-	json, err := FetchEcosystem()
+	json, err := FetchExternalEcosystem()
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: Implement schema validation here using schema from ECOSYSTEM_SCHEMA_URL.
-
-	ecosys, err := ParseEcosystemJSON(*json)
+	ecosys, err := ParseEcosystemJSON(json)
 	if err != nil {
-		return nil, err
+		// Fall back to file that the user will usually have cached.
+		json, err := FetchFallbackEcosystem()
+		if err != nil {
+			return nil, err
+		}
+
+		ecosys, err = ParseEcosystemJSON(json)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse fallback ecosystem.json file:\n%v", err)
+		}
+	} else {
+		ecosysPath, err := GetFallbackEcosystemPath()
+		if err != nil {
+			return nil, err
+		}
+
+		// No error, we can cache the valid JSON into fallback file.
+		dir := filepath.Dir(*ecosysPath)
+		if err := fileutil.MkDirAll(dir); err == nil {
+			_ = fileutil.WriteFile(*ecosysPath, json)
+		}
 	}
 
-	schema.validated = true
+	schema.ecosystem = ecosys
+	schema.validated = ecosys != nil // change when schema validation is implemented
+
 	return ecosys, nil
 }
 
-func ParseEcosystemJSON(body io.ReadCloser) (*ThunderstoreEcosystem, error) {
-	var parsed ThunderstoreEcosystem
-	if err := json.NewDecoder(body).Decode(&parsed); err != nil {
-		log.Printf("failed to decode JSON response of ecosystem.json\n%v", err)
+func GetFallbackEcosystemPath() (*string, error) {
+	cfgDir, err := os.UserConfigDir()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get path to fallback ecosystem.json file:\n%v", err)
+	}
+
+	path := filepath.Join(cfgDir, "modm8", "thunderstore", "ecosystem.json")
+	return &path, nil
+}
+
+// Reads the fallback ecosystem.json file from the user's config directory and returns its contents.
+//
+// This file is cached from a previous app launch where GetEcosystem() was successful and should usually exist.
+func FetchFallbackEcosystem() ([]byte, error) {
+	ecosysPath, err := GetFallbackEcosystemPath()
+	if err != nil {
 		return nil, err
+	}
+
+	json, err := os.ReadFile(*ecosysPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse fetched ecosystem and failed to read fallback file %s: %v", *ecosysPath, err)
+	}
+
+	return json, nil
+}
+
+// Downloads an external ecosystem.json file located at ECOSYSTEM_URL and returns its contents.
+func FetchExternalEcosystem() ([]byte, error) {
+	res, err := http.Get(ECOSYSTEM_URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch: %s\n%v", ECOSYSTEM_URL, err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch: %s\nstatus: %d", ECOSYSTEM_URL, res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func ParseEcosystemJSON(data []byte) (*ThunderstoreEcosystem, error) {
+	// TODO: Before we unmarshal, validate the schema is valid using file at ECOSYSTEM_SCHEMA_URL.
+	var parsed ThunderstoreEcosystem
+	err := json.Unmarshal(data, &parsed)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse ecosystem JSON: %v", err)
 	}
 
 	return &parsed, nil
-}
-
-func FetchEcosystem() (*io.ReadCloser, error) {
-	resp, err := http.Get(ECOSYSTEM_URL)
-	if err != nil || resp.StatusCode != 200 {
-		fmt.Printf("failed to download ecosystem.json from: %s", ECOSYSTEM_URL)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return &resp.Body, nil
 }

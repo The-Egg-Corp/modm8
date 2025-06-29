6876079b246847d69f791f52d66a16c8336a98d3
O3H <owen77stubbs@gmail.com>
2025-06-29 08:06:15 +0100

    dont rly care abt darwin anymore

--- backend/runners/steam/steam_windows.go
+++ backend/runners/steam/steam_windows.go
@@ -6,21 +6,21 @@ import "golang.org/x/sys/windows/registry"
 
 const platformExecName = "Steam.exe"
 
+var regPaths = []string{
+	`Software\Valve\Steam`,
+	`Software\WOW6432Node\Valve\Steam`,
+}
+
 func TryFindSteam() (*string, error) {
 	// Check Windows Registry for Steam installation path in both 32 and 64 bit paths.
-	paths := []string{
-		`Software\Valve\Steam`,
-		`Software\WOW6432Node\Valve\Steam`,
-	}
-
-	for _, regPath := range paths {
-		k, err := registry.OpenKey(registry.LOCAL_MACHINE, regPath, registry.QUERY_VALUE)
+	for _, path := range regPaths {
+		key, err := registry.OpenKey(registry.LOCAL_MACHINE, path, registry.QUERY_VALUE)
 		if err != nil {
 			continue
 		}
 
-		installPath, _, err := k.GetStringValue("InstallPath")
-		k.Close()
+		installPath, _, err := key.GetStringValue("InstallPath")
+		key.Close()
 
 		if err == nil {
 			return &installPath, nil
